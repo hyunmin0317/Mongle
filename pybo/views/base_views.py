287104bff6ebc1852f9e131b8706265ac82fd1a5
hyunmin0317 <choihm9903@naver.com>
2022-02-10 15:01:36 +0900

    feat: covid19 update

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -2,10 +2,48 @@ from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from django.db.models import Q, Count
 from ..models import Question
+import urllib.request
+from xml.etree.ElementTree import fromstring, ElementTree
+from elasticsearch import Elasticsearch, helpers
 import requests
 
 url = "52.78.99.246"
 index = "seoul_sample"
+
+def update():
+    es = Elasticsearch(['http://3.34.219.4:9200/'])
+
+    docs = []
+
+    url = 'http://openapi.seoul.go.kr:8088/547171685163686f35324270474f6e/xml/TbCorona19CountStatus/1/400/'
+    response = urllib.request.urlopen(url)
+    xml_str = response.read().decode('utf-8')
+
+    tree = ElementTree(fromstring(xml_str))
+    root = tree.getroot()
+
+    for row in root.iter("row"):
+        date = row.find('S_DT').text
+        today = int(row.find('N_HJ').text)
+        confirmed = int(row.find('T_HJ').text)
+        death = int(row.find('DEATH').text)
+        recover = int(row.find('RECOVER').text)
+        doc = {
+            "_index": "covid19_logstash",
+            "_id": date,
+            "_source": {
+                "date": date,
+                "today": today,
+                "confirmed": confirmed,
+                "death": death,
+                "recover": recover
+            }
+        }
+        docs.append(doc)
+
+    res = helpers.bulk(es, docs)
+    print("END")
+
 
 def home(request):
     page = request.GET.get('page', '1')  # 페이지
@@ -92,6 +130,7 @@ def bike(request):
     return render(request, 'pybo/bike.html')
 
 def covid19(request):
+    update()
     URL = "http://"+url+":9200/covid19_logstash/_search?sort=date:desc"
     data = requests.get(URL).json()['hits']['hits'][0]
     date = data['_source']['date']
