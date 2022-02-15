581627545dcfe52b23d3580d052e2a7ca892f819
hyunmin0317 <choihm9903@naver.com>
2022-02-15 19:17:53 +0900

    fix: staticfiles

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-yg^&i=#l)wy3#31*ny7mcddr*nq@&(j2s*!4^iotgub@-$i*sg
 
 ALLOWED_HOSTS = ["*"]
 
-DEBUG = True
+DEBUG = False
 
 # Application definition
 
@@ -122,6 +122,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
+STATIC_ROOT = os.path.join("staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
---
+++ staticfiles/script.js
@@ -0,0 +1,41 @@
+            // Some random colors
+const colors = ["#FED10A", "#7DE2D1", "#EF5098", "#17A8E0", "#93C83E", "#FED10A", "#7DE2D1", "#EF5098", "#17A8E0", "#93C83E","#FED10A", "#7DE2D1", "#EF5098", "#17A8E0", "#93C83E"];
+
+const numBalls = 10;
+const balls = [];
+
+for (let i = 0; i < numBalls; i++) {
+  let ball = document.createElement("div");
+  ball.classList.add("ball");
+  ball.style.background = colors[i];
+  ball.style.left = `${Math.floor(Math.random() * 85)}vw`;
+  ball.style.top = `${Math.floor(Math.random() * 80+10)}vh`;
+  ball.style.transform = `scale(${Math.random()})`;
+  ball.style.width = `${Math.random()*20}em`;
+  ball.style.height = ball.style.width;
+
+  balls.push(ball);
+  document.body.append(ball);
+}
+
+// Keyframes
+balls.forEach((el, i, ra) => {
+  let to = {
+    x: Math.random() * (i % 2 === 0 ? -0.5 : 0.5),
+    y: Math.random() * 2
+  };
+
+  let anim = el.animate(
+    [
+      { transform: "translate(0, 0)" },
+      { transform: `translate(${to.x}rem, ${to.y}rem)` }
+    ],
+    {
+      duration: (Math.random() + 1) * 2000, // random duration
+      direction: "alternate",
+      fill: "both",
+      iterations: Infinity,
+      easing: "ease-in-out"
+    }
+  );
+});
