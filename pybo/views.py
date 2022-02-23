6c570a4682e5e6df1020359251fe4d44757f6e7e
hyunmin0317 <choihm9903@naver.com>
2022-02-23 14:55:16 +0900

    feat: nft-search

--- pybo/views.py
+++ pybo/views.py
@@ -11,10 +11,40 @@ from pathlib import Path
 import json
 import urllib.request
 from pytrends.request import TrendReq
+from datetime import timedelta
 from dateutil.relativedelta import relativedelta
 
+def google_WORLD(word):
+    es = Elasticsearch(['http://3.34.219.4:9200/'])
+    docs = []
+    dict = {}
+
+    keywords = [word]
+    pytrends = TrendReq(hl='ko', tz=300)
+    pytrends.build_payload(keywords, cat=0, timeframe='today 1-m', geo='', gprop='')
+    by_region = pytrends.interest_by_region(resolution='COUNTRY', inc_geo_code=True)
+
+    key_list = by_region['geoCode'].to_list()
+    geo_list = by_region.index.to_list()
+    value_list = by_region['nft'].to_list()
+
+    for i in range(len(key_list)):
+        dict[key_list[i]] = [geo_list[i], value_list[i]]
+
+    for key in dict.keys():
+        doc = {
+            "_index": "world_nft",
+            "_id": key,
+            "_source": {
+                "country": dict[key][0],
+                "ratio": dict[key][1]
+            }
+        }
+        docs.append(doc)
+    res = helpers.bulk(es, docs)
+
 def naver_API(date):
-    delta = relativedelta(years=5)
+    delta = relativedelta(years=2)
     last = date - delta
     today = date.strftime("%Y-%m-%d")
     last_year = last.strftime("%Y-%m-%d")
@@ -396,6 +426,7 @@ def covid19(request):
 def nft(request):
     date1 = naver_NFT()
     date2 = google_NFT()
+    google_WORLD('nft')
     context = {"date1":date1, "date2":date2}
     return render(request, 'pybo/nft.html', context)
 
